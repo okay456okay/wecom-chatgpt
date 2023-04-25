3443254991e9bce26c7d2210508457d81db6238b
a04059 <xiulong.zhu@casstime.com>
2023-04-25 20:07:50 +0800

    fix wecom token expired issue

--- wecom.py
+++ wecom.py
@@ -3,20 +3,21 @@
 import requests
 from log import logger
 
+
 class WECOM_APP(object):
     def __init__(self, corp_id, agent_id, agent_secret):
         self.corp_id = corp_id
         self.agent_id = agent_id
         self.agent_secret = agent_secret
         self.s = requests.session()
-        self.access_token = self.get_token()
+        self.access_token = None
+        self.get_token()
 
     def get_token(self):
         url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={self.corp_id}&corpsecret={self.agent_secret}"
         r = self.s.get(url=url)
         logger.info(f"get access_token, url: {url}, response: {r.status_code}:{r.text}")
-        access_token = r.json()['access_token']
-        return access_token
+        self.access_token = r.json()['access_token']
 
     def txt_send2user(self, userid, text):
         url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={self.access_token}"
@@ -29,9 +30,10 @@ class WECOM_APP(object):
             },
             "safe": "0"
         }
-        r = requests.post(url=url, json=data)
+        r = self.s.post(url=url, json=data)
         logger.info(f"send message to user, url: {url}, data: {data}, response: {r.status_code}:{r.text}")
         if r.json().get('errcode', 0) == 42001:
             self.get_token()
-            r = requests.post(url=url, json=data)
+            url = f"https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={self.access_token}"
+            r = self.s.post(url=url, json=data)
             logger.info(f"send message to user, url: {url}, data: {data}, response: {r.status_code}:{r.text}")
