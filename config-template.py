f2e83eb01a4c0bace622940eaeee1d800b1b138b
a04059 <xiulong.zhu@casstime.com>
2023-04-27 17:48:22 +0800

    update config-template.py

--- config-template.py
+++ config-template.py
@@ -1,23 +1,23 @@
 # 我的企业 最下面
-corp_id = "ww69bda534f8fda184"
+corp_id = "ww69bd8fda184"
 # 在 应用管理 - 打开应用后，可查看 secret 和 agent_id
-agent_secret = "IGJMX3Ye71PfkGA0ETOF0WjMQd7Eivx1i34uzo8dFW0"
+agent_secret = "IGJMX3Ye71PfQd7Eivx1i34uzo8dFW0"
 agent_id = 1000002
 # 接收信息处设置
-token = "U6pfDj2kvxE4H9JnfxEZRr"
-encoding_aes_key = "EckrwxVr6XcMHQbwTWxWe4bQ2nRInF0PNyF9rDaXjQK"
-baidu_ocr_app_id = '32577953'
-baidu_ocr_api_key = 'llNjpb2PPGrmVhEzaCvGAyW1'
-baidu_ocr_api_secret = 'UW7W9ZFMFTh6evTS6Ae1neY8oyYNFCtW'
+token = "U6pfDj2nfxEZRr"
+encoding_aes_key = "EckrwxVr6e4bQ2nRInF0PNyF9rDaXjQK"
+baidu_ocr_app_id = '32553'
+baidu_ocr_api_key = 'llNjpb2PPhEzaCvGAyW1'
+baidu_ocr_api_secret = 'UW7W9Z6Ae1neY8oyYNFCtW'
 log_level = 'DEBUG'  # INFO
 # https://api2d.com/doc/doc
 openai_api_base = 'https://openai.api2d.net/v1'
-openai_api_key = 'fk198300-ggvcjDROuAD22IXOc0jolbDpZXMlcegr'
+openai_api_key = 'fk198300-ggvcjDROuA0jolbDpZXMlcegr'
 # openai_api_base = 'https://api.pawan.krd/v1'
-# openai_api_key = 'pk-VVsxGUhsbwfNQkOWYqkTxhVryrBcepKWjIfRZOpzTmKAmoaV' # discord ni126ni@gmail.com
+# openai_api_key = 'pk-VVsxGUhsbhVryrBcepKWjIfRZOpzTmKAmoaV' # discord ni126ni@gmail.com
 openai_proxy_enable = False
 # openai_api_base = 'https://api.openai.com/v1'
-# openai_api_key = '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' # openai
+# openai_api_key = 'sk-toKUib5xiW3BlbkFJKx6aXIQv3NkFjhRP3rdc' # openai
 # openai_proxy_enable = True
 openai_proxy = {
     'http': 'http://127.0.0.1:1087',
